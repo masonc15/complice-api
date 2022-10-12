d5c94387c671700c7153866c4a0be50b6748345e
Colin Mason <masonc789@gmail.com>
2022-10-12 15:26:54 -0400

    simplify build file

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 from setuptools import setup
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setup(
     name='complice-api',
-    version='0.1.0',
-    author='Complice',
-    author_email='team@complice.co',
+    version='0.1',
     description='A Python wrapper for the Complice API',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/complice/complice-api-python',
+    url='https://github.com/joshuacook/complice-api',
+    author='Colin Mason',
+    author_email='masonc789@gmail.com',
+    license='MIT',
     packages=['complice'],
-    install_requires=['requests'],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
+    zip_safe=False
 )
