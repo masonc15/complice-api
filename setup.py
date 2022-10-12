843df3fa6851c7019e4a7c5c2cfecf0f41a9af3f
Colin Mason <masonc789@gmail.com>
2022-10-12 15:40:37 -0400

    update repo url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='complice-api',
     version='0.1',
     description='A Python wrapper for the Complice API',
-    url='https://github.com/joshuacook/complice-api',
+    url='https://github.com/masonc15/complice-api',
     author='Colin Mason',
     author_email='masonc789@gmail.com',
     license='MIT',
